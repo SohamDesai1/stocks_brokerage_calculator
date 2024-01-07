d06db1ded030fa4cd36bd3bd27c0ded41c25960b
SohamDesai1 <sohamdesai123@gmail.com>
2024-01-07 14:14:01 +0530

    refactor: all charges and taxes till 2 decimal points

--- lib/utils/charges.dart
+++ lib/utils/charges.dart
@@ -10,14 +10,22 @@ class IntradayTrade {
     double b1 = (0.05 / 100) * totalSell;
     double b2 = 20;
     double brokerage = b1 > b2 ? b2 * 2 : b1 * 2;
-    double stt = totalSell * (0.025 / 100);
-    double sd = totalBuy * (0.003 / 100);
-    double etc = (totalBuy * (0.00325 / 100)) + (totalSell * (0.00325 / 100));
-    double sebi = (totalBuy * (0.0001 / 100)) + (totalSell * (0.0001 / 100));
-    double gst = ((18 / 100) * brokerage) + ((18 / 100) * etc);
+    double stt = double.parse((totalSell * (0.025 / 100)).toStringAsFixed(2));
+    double sd = double.parse((totalBuy * (0.003 / 100)).toStringAsFixed(2));
+    double etc = double.parse(
+        ((totalBuy * (0.00325 / 100)) + (totalSell * (0.00325 / 100)))
+            .toStringAsFixed(2));
+    double sebi = double.parse(
+        ((totalBuy * (0.0001 / 100)) + (totalSell * (0.0001 / 100)))
+            .toStringAsFixed(2));
+    double gst = double.parse(
+        (((18 / 100) * brokerage) + ((18 / 100) * etc) + ((18 / 100) * sebi))
+            .toStringAsFixed(2));
 
-    double totalTax = brokerage + stt + sd + etc + sebi + gst;
-    double profit = ((sellVal - buyVal) * quantityVal) - totalTax;
+    double totalTax = double.parse(
+        (brokerage + stt + sd + etc + sebi + gst).toStringAsFixed(2));
+    double profit = double.parse(
+        (((sellVal - buyVal) * quantityVal) - totalTax).toStringAsFixed(2));
 
     return [
       brokerage,
@@ -41,18 +49,24 @@ class OptionsTrade {
     double totalSell = quantityVal * sellVal;
     double totalBuy = quantityVal * buyVal;
 
-    double b1 = (0.05 / 100) * totalSell;
-    double b2 = 20;
-    double brokerage = b1 > b2 ? b2 * 2 : b1 * 2;
+    double brokerage = 20 + 20;
 
-    double stt = totalSell * (0.0625 / 100);
-    double sd = totalBuy * (0.003 / 100);
-    double etc = (totalBuy * (0.05 / 100)) + (totalSell * (0.05 / 100));
-    double sebi = (totalBuy * (0.0001 / 100)) + (totalSell * (0.0001 / 100));
-    double gst = ((18 / 100) * brokerage) + ((18 / 100) * etc);
+    double stt = double.parse((totalSell * (0.0625 / 100)).toStringAsFixed(2));
+    double sd = double.parse((totalBuy * (0.003 / 100)).toStringAsFixed(2));
+    double etc = double.parse(
+        ((totalBuy * (0.05 / 100)) + (totalSell * (0.05 / 100)))
+            .toStringAsFixed(2));
+    double sebi = double.parse(
+        ((totalBuy * (0.0001 / 100)) + (totalSell * (0.0001 / 100)))
+            .toStringAsFixed(2));
+    double gst = double.parse(
+        ((((18 / 100) * brokerage) + ((18 / 100) * etc) + ((18 / 100) * sebi)))
+            .toStringAsFixed(2));
 
-    double totalTax = brokerage + stt + sd + etc + sebi + gst;
-    double profit = ((sellVal - buyVal) * quantityVal) - totalTax;
+    double totalTax = double.parse(
+        (brokerage + stt + sd + etc + sebi + gst).toStringAsFixed(2));
+    double profit = double.parse(
+        (((sellVal - buyVal) * quantityVal) - totalTax).toStringAsFixed(2));
     return [
       brokerage,
       gst,
@@ -78,15 +92,25 @@ class DeliveryTrade {
     double b1 = (0.05 / 100) * totalSell;
     double b2 = 20;
     double brokerage = b1 > b2 ? b2 * 2 : b1 * 2;
-    
-    double stt = (totalBuy * (0.1 / 100)) + (totalSell * (0.1 / 100));
-    double sd = totalBuy * (0.015 / 100);
-    double etc = (totalBuy * (0.00325 / 100)) + (totalSell * (0.00325 / 100));
-    double sebi = (totalBuy * (0.0001 / 100)) + (totalSell * (0.0001 / 100));
-    double gst = ((18 / 100) * brokerage) + ((18 / 100) * etc);
+
+    double stt = double.parse(
+        ((totalBuy * (0.1 / 100)) + (totalSell * (0.1 / 100)))
+            .toStringAsFixed(2));
+    double sd = double.parse((totalBuy * (0.015 / 100)).toStringAsFixed(2));
+    double etc = double.parse(
+        ((totalBuy * (0.00325 / 100)) + (totalSell * (0.00325 / 100)))
+            .toStringAsFixed(2));
+    double sebi = double.parse(
+        ((totalBuy * (0.0001 / 100)) + (totalSell * (0.0001 / 100)))
+            .toStringAsFixed(2));
+    double gst = double.parse(((((18 / 100) * brokerage) +
+            ((18 / 100) * etc) +
+            ((18 / 100) * sebi) +
+            ((18 / 100) * 13.5)))
+        .toStringAsFixed(2));
 
     double totalTax = brokerage + stt + sd + etc + sebi + gst + 13.5;
-    double profit = ((sellVal - buyVal) * quantityVal) - totalTax;
+    double profit = (((sellVal - buyVal) * quantityVal) - totalTax);
 
     return [
       brokerage,
